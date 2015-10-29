67d8f4a5f9c1ce579295055d6758bf118c952648
dwipam <ddkatari@iu.edu>
2015-10-29 14:43:49 -0400

    remove duplicates

--- kl.R
+++
@@ -1,9 +0,0 @@
-bcd_test
-kl <- matrix( nrow = 9,ncol=9)
-col_arr <- c('V1','V2','V3','V4','V5','V6','V7','V8','V9')
-for (i in 1:9){
-  for (j in 1:9){
-    kl[i,j] <- KL.plugin(bcd_test[][,i],bcd_test[][,j])
-    
-  }
-}
