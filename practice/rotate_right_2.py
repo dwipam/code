5836e6798a3f4e9b4c63f181b78e1155330640b6
dwipam <dwipam@localhost-2.local>
2019-08-26 23:06:55 -0400

    Rotate right

--- practice/rotate_right_2.py
+++ practice/rotate_right_2.py
@@ -7,11 +7,8 @@
 a = [1,2,3,4]
 
 last_el = a[len(a)-1]
-temp = a[0]
-for i in range(1,len(a)):
-	temp_1 = a[i]
-	a[i] = temp
-	temp = temp_1
+for i in range(len(a)-1,0,-1):
+	a[i] = a[i-1]
 a[0] = last_el
 print a
 
